6ecfa0ca4b052dc08af77c030e09ea24967187e7
Andrei Catineanu <53002977+Andreiutz@users.noreply.github.com>
2024-02-26 23:00:30 +0200

    file upload with encryption

--- server/src/main/java/com/example/server/controller/TestController.java
+++ server/src/main/java/com/example/server/controller/TestController.java
@@ -8,16 +8,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/files")
 public class TestController {
 
     @PostMapping("/upload")
-    public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) throws IOException {
         System.out.println();
 
-        if (checkWavExtension(file)) {
-            return new ResponseEntity<>("File uploaded successfully!", HttpStatus.OK);
+        if (file != null && checkWavExtension(file)) {
+
+            String generatedFileName = UUID.randomUUID().toString();
+
+            String rootPath = System.getProperty("user.dir").replace("server", "data") + "\\audio\\" + generatedFileName + "\\";
+
+            Files.createDirectory(Paths.get(rootPath));
+
+            Files.copy(file.getInputStream(), Path.of(rootPath, "audio.wav"), StandardCopyOption.REPLACE_EXISTING);
+
+            return new ResponseEntity<>("File uploaded successfully! " + generatedFileName , HttpStatus.OK);
         } else {
             return new ResponseEntity<>("File type forbidden", HttpStatus.FORBIDDEN);
         }
